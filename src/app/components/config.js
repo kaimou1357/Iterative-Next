66b846baa7f13af11f8327f3074246019d2ea50f
Kai Mou <kaimou1357@gmail.com>
2023-12-28 22:25:25 -0800

    Remove WWW for API

--- src/app/components/config.js
+++ src/app/components/config.js
@@ -1,11 +1,11 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000/api"
-    : "https://www.api.goiterative.com/api";
+    : "https://api.goiterative.com/api";
 const SOCKET_IO_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://www.api.goiterative.com";
+    : "https://api.goiterative.com";
 const BASE_URL = SOCKET_IO_URL;
 
 export { API_BASE_URL, SOCKET_IO_URL, BASE_URL };
