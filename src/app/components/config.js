a9e99ec9cc1e9f4de8e47fe9202ee701b70bb580
Kai Mou <kaimou1357@gmail.com>
2023-12-28 22:24:32 -0800

    Update API Endpoint Values

--- src/app/components/config.js
+++ src/app/components/config.js
@@ -1,11 +1,11 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000/api"
-    : "https://www.goiterative.com/api";
+    : "https://www.api.goiterative.com/api";
 const SOCKET_IO_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://www.goiterative.com";
+    : "https://www.api.goiterative.com";
 const BASE_URL = SOCKET_IO_URL;
 
 export { API_BASE_URL, SOCKET_IO_URL, BASE_URL };
